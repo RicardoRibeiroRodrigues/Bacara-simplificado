208a1e3a758e327f400461701ba7597385576753
Ricardo <riywar@gmail.com>
2020-10-17 20:23:15 -0300

    Multiplos jogadores

--- Bacará.py
+++ Bacará.py
@@ -8,8 +8,10 @@ K = 0
 dez = 0
 #Baralhos usados.
 baralhos = [A, Q, K, J, 2 , 3, 4, 5, 6, 7, 8, 9, dez]*4*8   #8 baralhos  
+#jogadores
+n = int(input("Quantos jogadores serão?? "))
 #fichas iniciais
-fichas = 100
+fichas = [100]*n
 #testa para a rodada
 jogo = True 
 #funções a serem usadas depois.
@@ -23,42 +25,46 @@ def transforma_decimal(soma):
         return soma
 def fim_do_jogo(somaj,somab,fichas,jogo):
     if somaj == 8 or somaj == 9 or somab == 8 or somab == 9 or t == 1:
+        i = 0 #contador a ser usado
         #somaj maior que somab
         if somaj > somab:
-            if aposta_1 == "jogador":  #condição onde o j ganha
-                print('\033[32m' + "Voce venceu!" + '\033[0;0m')
-                fichas += aposta - aposta*0.0124   #1.24% de comissao
-                jogo = False
-                return fichas, jogo
-            if aposta_1 != "jogador":
-                print('\033[31m' + "Não foi dessa vez!" + '\033[0;0m')
-                fichas -= aposta
-                jogo = False
-                return fichas, jogo
+            while i < n:
+                if lista_apostas_ejb[i] == "jogador":  #condição onde o j ganha
+                    print('\033[32m' + f"Voce venceu jogador {i+1}!" + '\033[0;0m')
+                    fichas[i] += lista_apostas_num[i] - lista_apostas_num[i]*0.0124   #1.24% de comissao
+                    jogo = False #acaba a rodada
+                if lista_apostas_ejb[i] != "jogador":
+                    print('\033[31m' + f"Não foi dessa vez jogador {i+1}" + '\033[0;0m')
+                    fichas[i] -= lista_apostas_num[i]
+                    jogo = False #acaba a rodada
+                i += 1
+            return fichas, jogo
         #empate
         if somaj == somab:
-            if aposta_1 == "empate":  #condição onde o j ganha
-                print('\033[32m' + "Voce venceu!" + '\033[0;0m')
-                fichas += aposta*8 - aposta*0.1436   #14.36% de comissao  
-                jogo = False
-                return fichas, jogo
-            if aposta_1 != "empate":    #condição onde o j perde
-                print('\033[31m' + "Não foi dessa vez!" + '\033[0;0m')
-                fichas -= aposta
-                jogo = False
-                return fichas, jogo
+            while i < n:
+                if lista_apostas_ejb[i] == "empate":  #condição onde o j ganha
+                    print('\033[32m' + f"Voce venceu jogador {i+1}!" + '\033[0;0m')
+                    fichas[i] += lista_apostas_num[i]*8 - lista_apostas_num[i]*0.1436   #14.36% de comissao  
+                    jogo = False #acaba a rodada
+                if lista_apostas_ejb[i] != "empate":    #condição onde o j perde
+                    print('\033[31m' + f"Não foi dessa vez jogador {i+1}" + '\033[0;0m')
+                    fichas[i] -= lista_apostas_num[i] 
+                    jogo = False #acaba a rodada
+                i += 1
+            return fichas, jogo
         #somab maior que somaj
         if somab > somaj:
-            if aposta_1 == "banco":   #condição onde o j ganha
-                print('\033[32m' + "Voce venceu!" + '\033[0;0m')
-                fichas += aposta*0.95 - aposta*0.0106  #1.06% de comissao
-                jogo = False
-                return fichas, jogo
-            if aposta_1 != "banco":   #condição onde o j perde
-                print('\033[31m' + "Não foi dessa vez!" + '\033[0;0m')
-                fichas -= aposta
-                jogo = False
-                return fichas, jogo
+            while i < n:
+                if lista_apostas_ejb[i] == "banco":   #condição onde o j ganha
+                    print('\033[32m' + f"Voce venceu jogador {i+1}!" + '\033[0;0m')
+                    fichas[i] += lista_apostas_num[i]*0.95 - lista_apostas_num[i]*0.0106  #1.06% de comissao
+                    jogo = False #acaba a rodada
+                if lista_apostas_ejb[i] != "banco":   #condição onde o j perde
+                    print('\033[31m' + f"Não foi dessa vez jogador {i+1}" + '\033[0;0m')
+                    fichas[i] -= lista_apostas_num[i]
+                    jogo = False #acaba a rodada
+                i += 1
+            return fichas, jogo
     else:
         return fichas, jogo
 #regras avancadas para o banco comprar.
@@ -87,16 +93,28 @@ def terceira_carta_b(somaj,somab):
     else:
         return 0 
 #Jogo em si
-while fichas != 0:
-    print('\033[34m' + f"Você tem {fichas} fichas" + '\033[0;0m')
+while 0 not in fichas:
+    i = 0
+    while i < n:
+        print('\033[34m' + f"O jogador {i+1} tem {fichas[i]:.4f} fichas" + '\033[0;0m')
+        i += 1
     #Apostas
-    aposta = int(input("Quanto você quer apostar? "))
-    if aposta == 0 or aposta > fichas:   #Nao pode apostar 0 nem mais do que tem.
-        break
-    aposta_1 = input("No que voce aposta?(empate,jogador ou banco) ")
-    if aposta_1 != "empate" and aposta_1 != "jogador" and aposta_1 != "banco":  #deve apostar em uma das opções
-        break
-
+    i = 0
+    lista_apostas_num = [] #listas onde estarão as apostas numéricas de cada um dos jogadores
+    lista_apostas_ejb = [] #lista onde estarão em quem cada um dos jogadores apostou
+    while i < n:
+        aposta = int(input('\033[35m' + f"Quanto o jogador {i+1} vai apostar? " + '\033[0;0m'))
+        if aposta == 0 or aposta > fichas[i]:   #Nao pode apostar 0 nem mais do que tem.
+            break
+        lista_apostas_num.append(aposta)
+        i += 1
+    i = 0
+    while i < n:
+        aposta_1 = input('\033[35m' + f"No que o jogador {i+1} aposta?(empate,jogador ou banco) " + '\033[0;0m')
+        if aposta_1 != "empate" and aposta_1 != "jogador" and aposta_1 != "banco":  #deve apostar em uma das opções
+            break
+        lista_apostas_ejb.append(aposta_1)
+        i += 1
     #Cartas do jogador
     cartaj_1 = rnd.choice(baralhos)
     cartaj_2 = rnd.choice(baralhos)
@@ -112,7 +130,10 @@ while fichas != 0:
     #mudança de tipo do jogador para ajustar a soma (transformar decimais).
     somaj = transforma_decimal(somaj) 
     #mudança de tipo do banco para ajustar a soma (transformar decimais).
-    somab = transforma_decimal(somab)      
+    somab = transforma_decimal(somab)
+    #print das somas      
+    print(f"a soma do jogador vale {somaj}")
+    print(f"a soma do banco vale {somab}")
     #condicoes de acabar de primeira
     resultado_tuple = fim_do_jogo(somaj,somab,fichas,jogo)
     fichas = resultado_tuple[0]
@@ -138,5 +159,3 @@ while fichas != 0:
         resultado_tuple = fim_do_jogo(somaj,somab,fichas,jogo)
         fichas = resultado_tuple[0]
         jogo = resultado_tuple[1]
-
-
